6bb3d75ddd5dc429a54ecdfe840126bc3843a430
wenjunnie <1287018840@qq.com>
2021-06-07 17:22:20 +0800

    初始化项目

---
+++ controller/admin_login.go
@@ -0,0 +1,16 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type AdminLoginController struct {
+	
+}
+
+func AdminLoginRegister(group *gin.RouterGroup)  {
+	//adminLogin := &AdminLoginController{}
+	//group.
+}
+
+func (adminLogin *AdminLoginController) AdminLogin(c *gin.Context)  {
+	
+}
